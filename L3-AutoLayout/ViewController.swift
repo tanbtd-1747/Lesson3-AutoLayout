d4fe11647cdca0255f926562c120de7e1b495e90
Tran Duc Tan <tran.duc.tanb@framgia.com>
2019-02-20 14:01:02 +0700

    Initial Commit

---
+++ L3-AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L3-AutoLayout
+//
+//  Created by tran.duc.tan on 2/20/19.
+//  Copyright © 2019 tran.duc.tanb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
